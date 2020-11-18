ada0bfc44df371036b84bfc6819210fff36c9605
Hugo Caille <hugo@caille.me>
2020-11-18 22:23:16 +0100

    feat: 🎉 Initial commit of Quantcast choice packages

---
+++ packages/react-quantcast/src/TCFProvider.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { useMount } from 'react-use';
+import { PURPOSES } from './constants';
+import { TCFContext } from './contexts';
+
+export const getNonIABVendorConsents = () =>
+  new Promise((resolve, reject) =>
+    window.__tcfapi('getNonIABVendorConsents', 2, (data, success) => {
+      if (!success) {
+        return reject();
+      }
+      return resolve(data);
+    }),
+  );
+
+export const TCFProvider = ({
+  accountId,
+  customVendorsMapping = {},
+  children,
+}) => {
+  const [purposes, setPurposes] = useState([]);
+  const [customVendors, setCustomVendors] = useState([]);
+
+  const handleUserActionComplete = async tcData => {
+    const customConsents = await getNonIABVendorConsents();
+    const enabledPurposes = Object.keys(tcData.publisher.consents)
+      .filter(id => tcData.publisher.consents[id])
+      .map(id => PURPOSES[id]);
+
+    const customVendors = Object.keys(customConsents.nonIabVendorConsents)
+      .filter(id => customConsents.nonIabVendorConsents[id])
+      .map(id => customVendorsMapping[id]);
+
+    setPurposes(enabledPurposes);
+    setCustomVendors(customVendors);
+  };
+
+  const handleTCFReady = (tcData, success) => {
+    if (!success) {
+      return;
+    }
+    switch (tcData.eventStatus) {
+      case 'useractioncomplete':
+        handleUserActionComplete(tcData);
+        break;
+      default:
+        break;
+    }
+  };
+
+  useMount(() => {
+    if (!window.__tcfapi) {
+      return;
+    }
+    window.__tcfapi('addEventListener', 2, handleTCFReady);
+    return () => window.__tcfapi('removeEventListener', 2, handleTCFReady);
+  });
+
+  return (
+    <TCFContext.Provider value={{ purposes, customVendors }}>
+      {children({ purposes, customVendors })}
+    </TCFContext.Provider>
+  );
+};
---
+++ packages/react-quantcast/src/constants.js
@@ -0,0 +1,23 @@
+export const STORE_DATA = 'purpose.store_data';
+export const BASIC_ADS = 'purpose.basic_ads';
+export const ADS_PROFILING = 'purpose.ads_profiling';
+export const TARGETED_ADS = 'purpose.targeted_ads';
+export const CONTENT_PROFILING = 'purpose.content_profiling';
+export const TARGETED_CONTENT = 'purpose.targeted_content';
+export const AD_PERFORMANCE = 'purpose.ad_performance';
+export const CONTENT_PERFORMANCE = 'purpose.content_performance';
+export const MARKET_RESEARCH = 'purpose.market_research';
+export const IMPROVE_PRODUCTS = 'purpose.improve_products';
+
+export const PURPOSES = {
+  1: STORE_DATA,
+  2: BASIC_ADS,
+  3: ADS_PROFILING,
+  4: TARGETED_ADS,
+  5: CONTENT_PROFILING,
+  6: TARGETED_CONTENT,
+  7: AD_PERFORMANCE,
+  8: CONTENT_PERFORMANCE,
+  9: MARKET_RESEARCH,
+  10: IMPROVE_PRODUCTS,
+};
